a21dc7bafcbfc4835249b3f69992ba231ee9740c
0nik0nik0 <119619487+0nik0nik0@users.noreply.github.com>
2023-09-25 21:03:27 +0300

    HW_2_finished

---
+++ src/main/java/task1/Main.java
@@ -0,0 +1,27 @@
+//Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+// и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению
+// приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
+package task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean test = true;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while(test){
+            System.out.print("Type a float number: ");
+            try{
+                float floatNum = Float.parseFloat(reader.readLine());
+                System.out.printf("Your entered number is %f\n", floatNum);
+                test = false;
+            } catch (IOException|NumberFormatException e) {
+                System.out.println("Not float number, please try again!");
+            }
+
+        }
+    }
+}
---
+++ src/main/java/task4/Main.java
@@ -0,0 +1,24 @@
+//Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+// Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+
+package task4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+        public static void main(String[] args) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Type any text:");
+            try {
+                String result = reader.readLine();
+                if(result.equals("")) throw new RuntimeException("Empty string is not acceptable! Restart program to try again");
+
+                System.out.println("Your text is: " + result);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
